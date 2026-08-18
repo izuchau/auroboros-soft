c7a0883affe2bcb5cc95ffd03842084149d79f6d
MikhailG <mixplus13@mail.ru>
2026-08-18 13:20:54 +1000

    Add Yandex Metrika via cookie consent

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,12 +5,14 @@ interface Window {
     reset: (widgetId?: number) => void;
     getResponse: (widgetId?: number) => string;
   };
+  ym?: ((counterId: number, method: string, ...args: unknown[]) => void) & { a?: unknown[]; l?: number };
 }
 
 interface ImportMetaEnv {
   readonly PUBLIC_FORMSPREE_ID: string;
   readonly PUBLIC_RECAPTCHA_SITE_KEY: string;
   readonly PUBLIC_GA_MEASUREMENT_ID: string;
+  readonly PUBLIC_YANDEX_METRIKA_ID: string;
 }
 
 interface ImportMeta {
